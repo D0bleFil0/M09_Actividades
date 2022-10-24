5474982ca8e09fd45990876a4ae5a2703de6b755
D0bleFil0 <albertoperezdelrio@gmail.com>
2022-10-24 19:56:20 +0200

    first commit

---
+++ src/Actividad_1_ejercicio_1/Conversor.java
@@ -0,0 +1,36 @@
+package Actividad_1_ejercicio_1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Conversor {
+
+    public static void main(String[] args) throws Exception {
+
+        // Comunicacion entre proceso padre y proceso hijo
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lineaEnvio = new String();
+        lineaEnvio = br.readLine();
+
+        Integer valor = Integer.parseInt(lineaEnvio);
+        String resultado = decimalABinario(valor);
+        System.out.println(resultado);
+    }
+
+    public static String decimalABinario(int numero) {
+        String binario = "";
+        while (numero > 0) {
+            if (numero % 2 == 0) {
+                binario = "0" + binario;
+            } else {
+                binario = "1" + binario;
+            }
+            numero = numero / 2;
+        }
+        // completar con ceros a la izquierda
+        int longitud = binario.length();
+        for (int i = 0; i < 4 - longitud; i++) {
+            binario = "0" + binario;
+        }
+        return binario;
+    }
+}
---
+++ src/Actividad_1_ejercicio_1/Principal.java
@@ -0,0 +1,49 @@
+package Actividad_1_ejercicio_1;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class Principal {
+
+    public static void banner() {
+        System.out.printf("Conversor Decimal a Binario\n");
+        System.out.printf("[Escribe FIN para terminar]\n");
+    }
+    public static void main(String[] args) throws Exception {
+
+        // Bienvenida
+        banner();
+
+        // Comunicacion entre proceso padre y proceso hijo
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lineaEnvio = new String();
+        lineaEnvio = br.readLine();
+
+       loop: while (true) {
+            // Creacion del proceso hijo
+            ProcessBuilder pb = new ProcessBuilder("java","-jar","conversor.jar");
+            Process p = pb.start();
+
+            // Comunicacion entre proceso padre y proceso hijo
+            OutputStream os = p.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.println(lineaEnvio);
+            pw.flush();
+
+            // Comunicacion entre proceso padre y proceso hijo
+            InputStream is = p.getInputStream();
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(is));
+            String lineaRecibida = br2.readLine();
+            System.out.println(lineaRecibida);
+
+            // Comunicacion entre proceso padre y proceso hijo
+            lineaEnvio = br.readLine();
+
+            if (lineaEnvio.equalsIgnoreCase("fin")) {
+                break loop;
+            }
+        }
+    }
+}
