2a35f72a42ee4e1952f07f881a7c0b65f84a51c2
D0bleFil0 <albertoperezdelrio@gmail.com>
2022-10-25 19:48:27 +0200

    update syso messages

--- src/Actividad_1_ejercicio_3/Pizzeria.java
+++ src/Actividad_1_ejercicio_3/Pizzeria.java
@@ -28,7 +28,9 @@ public class Pizzeria {
         // Metodo offer para agregar pizzas a la cola
         pizzas.offer(unidades);
         //Imprime las pizzas que hay en la cola
-        System.out.printf("\n[Hay " + pizzas.size() + " pizzas en la cola]\n");
+        System.out.printf("------------------------------\n");
+        System.out.printf("Pizza(s) en cola: %d", pizzas.size());
+        System.out.printf("\n------------------------------\n");
         // Notificacion de todos los hilos para que se ejecuten en orden de llegada a la cola
         notifyAll();
         // Imprime el nombre del hilo y la cantidad de pizzas producidas
