46729da7616876fd99b1a83108329d6b29bc054c
k3ssdev <albertoperezdelrio@gmail.com>
2022-12-09 19:43:30 +0100

    created server and client classes

--- src/Actividad_2_ejercicio_1/ClienteAhorcado.java
+++ src/Actividad_2_ejercicio_1/ClienteAhorcado.java
@@ -1,66 +1,67 @@
-// Cliente.java 
+// juego del ahorcado cliente-servidor
 
 package Actividad_2_ejercicio_1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClienteAhorcado {
 
-	private static Socket clientSocket;
+    public static void main(String[] args) {
+        String line;
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		String line;
-		// definimos el socket
-		clientSocket = new Socket();
-		// Creamos un Socket Adress para una máquina y numero de puerto
-		InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
-		try {
-			System.out.println("**CONVERTIDOR DE DECIMAL A BINARIO - CLIENTE**");
-			// Conectamos !!!
-			clientSocket.connect(addr);
-			// Envio la informacion a traves del canal de comunicacion establecido con el
-			// socket
-			PrintStream ps = new PrintStream(clientSocket.getOutputStream(), true);
-			// Lee la informacion del teclado para enviarla al servidor
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-			System.out.println("-Conexión establecida-");
-			System.out.println();
-			System.out.println("Introduce un numero o escribe FIN para salir");
-			System.out.println();
-			// Lee la informacion del teclado para enviarla al servidor
-			line = in.readLine();
-			// Condicional para que solo pueda enviar numeros o la palabra FIN
-			while (!line.equalsIgnoreCase("FIN")) {
-				if (line.matches("[0-9]+")) {
-					ps.println(line);
-					// Creamos un buffer para leer los datos que nos envia el servidor
-					BufferedReader br = new BufferedReader(
-							new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
-					// Leemos el numero que nos envia el servidor
-					line = br.readLine();
-					System.out.println("Enviado: " + line);
-					System.out.println("Recibido: " + line);
-					System.out.println();
-					// Lee la informacion del teclado para enviarla al servidor
-					line = in.readLine();
-				} else {
-					System.out.println("Solo puedes enviar numeros o la palabra FIN");
-					System.out.println();
-					// Lee la informacion del teclado para enviarla al servidor
-					line = in.readLine();
-				}
-			}
-			// Cerramos el socket
-			clientSocket.close();
-			System.out.println("Conexión cerrada");
-			// Capturamos las excepciones
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-	}
+        try {
+            // Creamos un socket con el que nos conectaremos al servidor
+            Socket socket = new Socket();
+
+            // Creamos un Socket Adress para una máquina y numero de puerto
+            InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+
+            // Asignamos el socket a la direccion
+            socket.connect(addr);
+
+            // Creamos un buffer para leer los datos que nos envia el servidor
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+            // Creamos un try para leer los datos que nos envia el servidor
+            try {
+                // Leemos la palabra que nos envia el servidor
+                line = br.readLine();
+
+                // Mientras no sea “fin”
+                while (!line.equalsIgnoreCase("FIN")) {
+                    System.out.println("Recibido: " + line);
+
+                    // Enviamos la letra
+                    System.out.println("Enviado: ");
+                    String letra = sc.nextLine();
+                    PrintStream ps = new PrintStream(socket.getOutputStream(), true);
+                    ps.println(letra);
+                    ps.flush();
+
+                    // Leemos la palabra que nos envia el servidor
+                    line = br.readLine();
+
+                }
+
+            } catch (NullPointerException ex) {
+                System.out.println("Finalizada transmision…");
+            }
+            // cerramos conexion
+            socket.close();
+            System.out.println("Fin");
+
+        } catch (IOException e) {
+            System.out.println("OOOPS!!!");
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/Actividad_2_ejercicio_1/ServidorAhorcado.java
+++ src/Actividad_2_ejercicio_1/ServidorAhorcado.java
@@ -8,26 +8,178 @@ import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.FileReader;
 
 public class ServidorAhorcado {
 
     // Funcion para convertir un numero decimal a binario
-    public static String decimalABinario(int numero) {
-        String binario = "";
-        while (numero > 0) {
-            if (numero % 2 == 0) {
-                binario = "0" + binario;
-            } else {
-                binario = "1" + binario;
-            }
-            numero = numero / 2;
-        }
-        // Bloque para completar con ceros a la izquierda
-        int longitud = binario.length();
-        for (int i = 0; i < 4 - longitud; i++) {
-            binario = "0" + binario;
-        }
-        return binario;
+    public static String ahorcado(String letra) {
+             // Crear un array de palabras con 100 posiciones posibles
+             String[] palabras = new String[100];
+             // Variable para guardar el indice del array
+             int indice = 0;
+     
+             // Leer el fichero de palabras y guardarlas en el array
+             try {
+                 // Obtener ruta del fichero
+                 String ruta = ServidorAhorcado.class.getResource("diccionario.txt").getPath();
+                 // Leer el fichero
+                 BufferedReader br = new BufferedReader(new FileReader(ruta));
+                 // Leer la primera linea
+                 String linea = br.readLine();
+                 // Leer el resto de lineas y guardarlas en el array mientras no sea null
+                 while (linea != null) {
+                     palabras[indice] = linea;
+                     System.out.println(palabras[indice]);
+                     indice++;
+                     linea = br.readLine();
+                 }
+                 // Cerrar el fichero
+                 br.close();
+                 // Capturar errores
+             } catch (Exception e) {
+                 System.out.println("Error: " + e.getMessage());
+             }
+             // Eliminar las posiciones del array que sean null
+             String[] palabras2 = new String[indice];
+             for (int i = 0; i < indice; i++) {
+                 palabras2[i] = palabras[i];
+             }
+             palabras = palabras2;
+     
+             // Elegir una palabra aleatoria del diccionario
+             Random r = new Random();
+             indice = r.nextInt(palabras.length);
+             String palabra = palabras[indice];
+     
+             // Crear un array de letras con tantas posiciones como letras tenga la palabra
+             String[] letras = new String[palabra.length()];
+     
+             // Crear un array de strings para dibujar el ahorcado
+             String[] ahorcado = new String[6];
+             ahorcado[0] = "  +---+";
+             ahorcado[1] = "  |   |";
+             ahorcado[2] = "      |";
+             ahorcado[3] = "      |";
+             ahorcado[4] = "      |";
+             ahorcado[5] = "========";
+     
+             // Inicializar el array de letras con guiones bajos
+             for (int i = 0; i < letras.length; i++) {
+                 letras[i] = "_";
+             }
+     
+             // Bucle principal del juego
+             Scanner sc = new Scanner(System.in);
+             boolean victoria = false;
+             int intentos = 7;
+             // Cierra el bucle cuando se acaben los intentos o se acierte la palabra
+             do {
+                 // borrar la pantalla y dibujar el ahorcado
+                 System.out.print("\033[H\033[2J");
+                 System.out.println("Intentos: " + intentos);
+                 System.out.println("");
+                 for (int i = 0; i < ahorcado.length; i++) {
+                     System.out.println(ahorcado[i]);
+                 }
+                 for (int i = 0; i < letras.length; i++) {
+                     System.out.print(letras[i] + " ");
+                 }
+                 // Pedir una letra
+                 System.out.println("");
+                 System.out.println("");
+                 System.out.println("Introduce una letra:");
+                 String letra = sc.nextLine();
+                 boolean acierto = false;
+                 // Comprobar si la letra está en la palabra
+                 for (int i = 0; i < palabra.length(); i++) {
+                     if (letra.equals(palabra.substring(i, i + 1))) {
+                         letras[i] = letra;
+                         acierto = true;
+                     }
+                 }
+     
+                 // Si no se ha acertado la letra, se dibuja una parte del ahorcado usando
+                 // case-switch
+                 if (!acierto) {
+                     intentos--;
+                     switch (intentos) {
+                         case 6:
+                             ahorcado[2] = "  O   |";
+                             break;
+                         case 5:
+                             ahorcado[3] = "  |   |";
+                             break;
+                         case 4:
+                             ahorcado[3] = " /|   |";
+                             break;
+                         case 3:
+                             ahorcado[3] = " /|\\  |";
+                             break;
+                         case 2:
+                             ahorcado[4] = " /    |";
+                             break;
+                         case 1:
+                             ahorcado[4] = " / \\  |";
+                             break;
+                     }
+                 }
+                 // Comprobar si se ha acertado la palabra y dibujar el ahorcado completo
+                 victoria = true;
+                 for (int i = 0; i < letras.length; i++) {
+                     if (letras[i].equals("_")) {
+                         victoria = false;
+                     }
+                 }
+                 // Cierra el bucle cuando se acaben los intentos o se acierte la palabra
+             } while (intentos > 0 && !victoria);
+             if (victoria) {
+                 // Borrar la pantalla
+                 System.out.print("\033[H\033[2J");
+                 // Dibujar ahorcado
+                 for (int i = 0; i < ahorcado.length; i++) {
+                     System.out.println(ahorcado[i]);
+                 }
+                 for (int i = 0; i < letras.length; i++) {
+                     System.out.print(letras[i] + " ");
+                 }
+                 // Mensaje de victoria
+                 System.out.println("");
+                 System.out.println("");
+                 System.out.println("¡Has ganado!");
+                 System.out.println("");
+                 // Reinicia partida
+                 System.out.println("¿Quieres jugar otra vez? (s/n)");
+                 String respuesta = sc.nextLine();
+                 // volver a jugar si es igual a s ignorar mayusculas y minusculas
+                    if (respuesta.equalsIgnoreCase("s")) {
+                        main(ahorcado);
+                    }
+     
+             } else {
+                 // Borrar la pantalla y mostrar mensaje de derrota con el ahorcado completo
+                 System.out.print("\033[H\033[2J");
+                 System.out.println("¡Has muerto!");
+                 System.out.println("");
+                 // Dibujar ahocardo completo
+                 for (int i = 0; i < ahorcado.length; i++) {
+                     System.out.println(ahorcado[i]);
+                 }
+                 System.out.print("\nLa palabra era: " + palabra);
+                 System.out.println("");
+                 System.out.println("");
+                 // Reinicia partida
+                 System.out.println("¿Quieres jugar otra vez? (s/n)");
+                 String respuesta = sc.nextLine();
+                 sc.close();
+                 // si es igual a s ignorar mayusculas y minusculas
+                 if (respuesta.equalsIgnoreCase("s")) {
+                     main(ahorcado);
+                 }
+             }
+        //return palabraOculta;
     }
 
     private static ServerSocket serverSocket;
@@ -82,3 +234,6 @@ public class ServidorAhorcado {
     }
 
 }
+
+
+// juego del ahorcado cliente-servidor
