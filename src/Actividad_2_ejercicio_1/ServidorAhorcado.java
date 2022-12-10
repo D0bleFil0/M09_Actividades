1936c2be6a14c9c9689baa8f6dd3ef6e02699a44
k3ssdev <albertoperezdelrio@gmail.com>
2022-12-10 16:13:33 +0100

    working on server

--- src/Actividad_2_ejercicio_1/ServidorAhorcado.java
+++ src/Actividad_2_ejercicio_1/ServidorAhorcado.java
@@ -1,2 +1,120 @@
 // Servidor.java
 package Actividad_2_ejercicio_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+
+public class ServidorAhorcado {
+
+    // Creamos un objeto de la clase ServerSocket para el servidor 
+    private static ServerSocket serverSocket;
+
+    
+
+    public static void main(String[] args) {
+
+        // Crear un array de palabras con 100 posiciones posibles
+    String[] palabras = new String[100];
+    // Variable para guardar el indice del array
+    int indice = 0;
+
+    // Leer el fichero de palabras y guardarlas en el array
+    try {
+        // Obtener ruta del fichero
+        String ruta = Ahorcado.class.getResource("diccionario.txt").getPath();
+        // Leer el fichero
+        BufferedReader br = new BufferedReader(new FileReader(ruta));
+        // Leer la primera linea
+        String linea = br.readLine();
+        // Leer el resto de lineas y guardarlas en el array mientras no sea null
+        while (linea != null) {
+            palabras[indice] = linea;
+            System.out.println(palabras[indice]);
+            indice++;
+            linea = br.readLine();
+        }
+        // Cerrar el fichero
+        br.close();
+        // Capturar errores
+    } catch (Exception e) {
+        System.out.println("Error: " + e.getMessage());
+    }
+    // Eliminar las posiciones del array que sean null
+    String[] palabras2 = new String[indice];
+    for (int i = 0; i < indice; i++) {
+        palabras2[i] = palabras[i];
+    }
+    palabras = palabras2;
+
+    // Elegir una palabra aleatoria del diccionario
+    Random r = new Random();
+    indice = r.nextInt(palabras.length);
+    String palabra = palabras[indice];
+
+    // Crear un array de letras con tantas posiciones como letras tenga la palabra
+    String[] letras = new String[palabra.length()];
+
+    // Crear un array de strings para dibujar el ahorcado
+    String[] ahorcado = new String[6];
+    ahorcado[0] = "  +---+";
+    ahorcado[1] = "  |   |";
+    ahorcado[2] = "      |";
+    ahorcado[3] = "      |";
+    ahorcado[4] = "      |";
+    ahorcado[5] = "========";
+
+    // Inicializar el array de letras con guiones bajos
+    for (int i = 0; i < letras.length; i++) {
+        letras[i] = "_";
+    }
+
+    // Bloque de codigo para el servidor
+
+        String line;
+º       
+        // Se crea la conexion
+        try {
+            // Utilizamos la clase ServerSocket en el servidor
+            serverSocket = new ServerSocket();
+            System.out.println("**JUEGO DEL AHORCADO - SERVIDOR**");
+            System.out.println("Esperando Conexion...");
+            // Creamos un Socket Adress para una máquina y numero de puerto
+            InetSocketAddress addr = new InetSocketAddress("localhost", 5050);
+            // Asignamos el socket a la direccion
+            serverSocket.bind(addr);
+            // Aceptamos la conexion
+            Socket socket = serverSocket.accept();
+            System.out.println("Cliente conectado...");
+            System.out.println();
+            // Creamos un buffer para leer los datos que nos envia el cliente
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+            //Recibimos la palabra del cliente
+            // Creamos un try para leer los datos que nos envia el cliente
+            try {
+                // Leemos el numero que nos envia el cliente
+                line = br.readLine();
+                
+
+                
+            } catch (NullPointerException ex) {
+                System.out.println("Finalizada transmision...");
+            }
+            // cerramos conexion
+            socket.close();
+            System.out.println("Fin.");
+
+        } catch (IOException e) {
+            System.out.println("OOOPS !!!");
+            e.printStackTrace();
+        }
+    }
+}
+
