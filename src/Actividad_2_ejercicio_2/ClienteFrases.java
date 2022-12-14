a09bd6a56e7befedd17fe14c00c8bfee9f7ca8b4
k3ssdev <albertoperezdelrio@gmail.com>
2022-12-14 20:04:06 +0100

    working on client code

--- src/Actividad_2_ejercicio_2/ClienteFrases.java
+++ src/Actividad_2_ejercicio_2/ClienteFrases.java
@@ -4,21 +4,18 @@ import java.rmi.registry.LocateRegistry;
 
 public interface ClienteFrases {
 
-    private ClienteFrases() {
-    }
+    private ClienteFrases() {}
 
     public static void main(String[] args) {
         // Crea un try-catch para capturar las excepciones
         try {
             // Se obtiene el registro en el puerto 1099
             Registry registry = LocateRegistry.getRegistry(1099);
-            // Se obtiene el stub del objeto remoto que se encuentra en el registro
-            Frases stub = (Frases) registry.lookup("Frases");
+            // Se busca el objeto remoto
+            ServidorFrases.Frases stub = (ServidorFrases.Frases) registry.lookup("Frases");
             // Se llama al método remoto
-            String response = stub.getFrase();
-            //
-            System.out.println("response: " + response);
+            String frase = stub.getFrase();
+            // Se muestra el resultado
+            System.out.println("Frase: " + frase);
         } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
-}
+            
