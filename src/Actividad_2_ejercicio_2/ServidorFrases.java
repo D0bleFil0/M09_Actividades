98aa4fe7a1ace33d60edfc602866cedfdb56667d
k3ssdev <albertoperezdelrio@gmail.com>
2022-12-14 20:16:07 +0100

    comments added

--- src/Actividad_2_ejercicio_2/ServidorFrases.java
+++ src/Actividad_2_ejercicio_2/ServidorFrases.java
@@ -13,18 +13,23 @@ public class ServidorFrases {
 
     // Crea la interfaz remota
     public interface Frases extends Remote {
+        // Declaración del método remoto
         public String getFrase() throws RemoteException;
     }
 
-    // Implementa la interfaz remota
-
+    // Implementación la interfaz remota
     public static class FrasesImpl implements Frases {
+        // Frases que se van a devolver
         private String[] frases = {"La vida es bella", "El que la sigue la consigue", "El que madruga, dios le ayuda"};
+        // Contador para recorrer el array
         private int contador = 0;
+        // Implementación del método remoto
         public String getFrase() throws RemoteException {
+            // Si el contador llega al final del array, se reinicia
             if (contador == frases.length) {
                 contador = 0;
             }
+            // Se devuelve la frase y se incrementa el contador
             return frases[contador++];
         }
     }
