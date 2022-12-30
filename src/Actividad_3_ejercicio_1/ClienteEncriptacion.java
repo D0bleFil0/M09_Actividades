fdc6b26a2178973d9670e4c1806524ea4862e971
D0bleFil0 <albertoperezdelrio@gmail.com>
2022-12-30 09:56:09 +0100

    files created using actividad 2

---
+++ src/Actividad_3_ejercicio_1/ClienteEncriptacion.java
@@ -0,0 +1,64 @@
+package Actividad_3_ejercicio_1;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+// Crea la clase cliente para ServidorFrases que implementa la interfaz remota
+public class ClienteEncriptacion {
+
+    // Metodo principal
+    public static void main(String[] args) {
+        try {
+            // Crea el registro en el puerto 1099
+            Registry registro = LocateRegistry.getRegistry(1099);
+            // Crea el stub
+            ServidorEncriptacion.Frases stub = (ServidorEncriptacion.Frases) registro.lookup("Frases");
+            // Crea el objeto Scanner
+            Scanner sc = new Scanner(System.in);
+
+            String frase = "";
+
+            boolean salir = false;
+
+            while (!salir) {
+                // Borra la pantalla
+                System.out.print("\033[H\033[2J");
+                // Llama al método remoto de menu
+                System.out.println(stub.getMenu());
+                // Pide al usuario que introduzca una opción
+                System.out.print("\n Introduce una opción: ");
+                String opcion = sc.nextLine();
+                // Si la opción es F, sale del programa
+                if (opcion.equalsIgnoreCase("F")) {
+                    System.out.println(" Saliendo del programa...");
+                    stub.elegirOpcion(opcion, "F");
+                    salir = true;
+                } else {
+                    // Pide al usuario que introduzca una frase
+                    System.out.print("\n Introduce una frase: ");
+                    frase = sc.nextLine();
+
+                    // Llama al metodo remoto de elegirOpcion
+                    String resultado = stub.elegirOpcion(opcion, frase);
+                    // Muestra el resultado
+                    System.out.println("");
+                    System.out.println(resultado);
+                    System.out.println("\n");
+
+                    // Pide al usuario que pulse intro para continuar
+                    System.out.println(" Pulsa intro para continuar...");
+                    sc.nextLine();
+                }
+
+            }
+
+            // Cierra el objeto Scanner
+            sc.close();
+
+        } catch (Exception e) {
+            System.err.println("Excepción del cliente: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Actividad_3_ejercicio_1/ServidorEncriptacion.java
@@ -0,0 +1,188 @@
+package Actividad_3_ejercicio_1;
+
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public class ServidorEncriptacion {
+
+    // Metodo principal
+    public static void main(String[] args) throws RemoteException {
+
+        try {
+            // Crea el registro en el puerto 1099
+            Registry registro = LocateRegistry.createRegistry(1099);
+            // Instancia el objeto remoto
+            FrasesImpl obj = new FrasesImpl();
+            // Crea el stub
+            Frases stub = (Frases) UnicastRemoteObject.exportObject(obj, 0);
+            // Lo registra en el registro
+            registro.rebind("Frases", stub);
+            // Borra la pantalla
+            System.out.print("\033[H\033[2J");
+            // Notifica que el servidor está preparado
+            System.out.println(" ***Servidor preparado***");
+            System.out.println("");
+            System.out.println("");
+            System.out.println(" Pulse control + c para salir del programa");
+            // Muestra lo que va haciendo el cliente
+            System.out.println(" Esperando peticiones del cliente...");
+            System.out.println("");
+            
+            // Mostrar registro de lo que va haciendo el cliente
+            System.out.println(" Registro de peticiones realizadas por cliente:");
+            System.out.println("");
+            
+
+            // Si se produce una excepción, la muestra
+        } catch (Exception e) {
+            System.err.println(" Excepción del servidor: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    // Crea la interfaz remota
+    public interface Frases extends Remote {
+        // Crea los métodos remotos
+        public String getMenu() throws RemoteException;
+
+        public String longitudFrase(String frase) throws RemoteException;
+
+        public String numeroPalabras(String frase) throws RemoteException;
+
+        public String numeroVocales(String frase) throws RemoteException;
+
+        public String fraseInvertida(String frase) throws RemoteException;
+
+        public String fraseMayusculas(String frase) throws RemoteException;
+
+        public String fraseMinusculas(String frase) throws RemoteException;
+
+        public String elegirOpcion(String opcion, String frase) throws RemoteException;
+    }
+
+    // Crea la clase que implementa la interfaz remota
+    public static class FrasesImpl implements Frases {
+
+        // Crea método remoto para menu, al que se le pasa la frase y devuelve el
+        // resultado
+        public String getMenu() throws RemoteException {
+
+            String[] menu = { " (L) Longitud de la frase, ",
+                    "(P) Número de palabras de la frase, ", "(V) Número de vocales de la frase, ",
+                    "(I) Frase invertida, ", "(M) Frase en mayúsculas, ", "(m) Frase en minúsculas, ",
+                    "(F) Salir del programa " };
+            String menuString = "";
+            for (int i = 0; i < menu.length; i++) {
+                menuString += menu[i] + "";
+            }
+
+            return menuString;
+        }
+
+        // Metodo remoto para elegir la opcion y devuelve el resultado
+        public String elegirOpcion(String opcion, String frase) throws RemoteException {
+            String resultado = "";
+            switch (opcion) {
+                case "L":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Longitud de la frase: " + longitudFrase(frase));
+                    resultado = " Longitud de la frase: " + longitudFrase(frase);
+                    break;
+                case "P":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Número de palabras: " + numeroPalabras(frase));
+                    resultado = " Número de palabras: " + numeroPalabras(frase);
+                    break;
+                case "V":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Número de vocales: " + numeroVocales(frase));
+                    resultado = " Número de vocales: " + numeroVocales(frase);
+                    break;
+                case "I":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Frase invertida: " + fraseInvertida(frase));
+                    resultado = " Frase invertida: " + fraseInvertida(frase);
+                    break;
+                case "M":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Frase en mayúsculas: " + fraseMayusculas(frase));
+                    resultado = " Frase en mayúsculas: " + fraseMayusculas(frase);
+                    break;
+                case "m":
+                    System.out.println(" Frase cliente: " + frase);
+                    System.out.println(" Frase en minúsculas: " + fraseMinusculas(frase));
+                    resultado = " Frase en minúsculas: " + fraseMinusculas(frase);
+                    break;
+                case "F":
+                    System.out.println("\n ¡Cliente desconectado!");
+                    resultado = " Fin del programa";
+                    break;
+                case "f":
+                    System.out.println("\n ¡Cliente desconectado!\n");
+                    resultado = " Fin del programa";
+                    break;
+                default:
+                    System.out.println(" Opción incorrecta");
+                    resultado = " Opción incorrecta";
+                    break;
+            }
+
+            return resultado;
+        }
+
+        // Metodo para longitud de la frase
+        public String longitudFrase(String frase) throws RemoteException {
+            String longitud = Integer.toString(frase.length());
+            return longitud;
+        }
+
+        // Metodo para numero de palabras de la frase
+        public String numeroPalabras(String frase) throws RemoteException {
+            int numeroPalabras = 0;
+            for (int i = 0; i < frase.length(); i++) {
+                if (frase.charAt(i) == ' ') {
+                    numeroPalabras++;
+                }
+            }
+            String numeroPalabras2 = Integer.toString(numeroPalabras + 1);
+            return numeroPalabras2;
+        }
+
+        // Metodo para numero de vocales de la frase
+        public String numeroVocales(String frase) throws RemoteException {
+            int numeroVocales = 0;
+            for (int i = 0; i < frase.length(); i++) {
+                if (frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o'
+                        || frase.charAt(i) == 'u') {
+                    numeroVocales++;
+                }
+            }
+            String numeroVocales2 = Integer.toString(numeroVocales);
+            // Devuelve el numero de vocales en string
+            return numeroVocales2;
+        }
+
+        // Metodo para frase invertida
+        public String fraseInvertida(String frase) throws RemoteException {
+            String fraseInvertida = "";
+            for (int i = frase.length() - 1; i >= 0; i--) {
+                fraseInvertida += frase.charAt(i);
+            }
+            return fraseInvertida;
+        }
+
+        // Metodo para frase en mayusculas
+        public String fraseMayusculas(String frase) throws RemoteException {
+            return frase.toUpperCase();
+        }
+
+        // Metodo para frase en minusculas
+        public String fraseMinusculas(String frase) throws RemoteException {
+            return frase.toLowerCase();
+        }
+
+    }
+}
